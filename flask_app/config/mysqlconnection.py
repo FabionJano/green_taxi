0e8b176ebd3f36923589a9d3650589257fc02a07
Fabion Jano <fabjonjano@gmail.com>
2024-02-20 13:18:06 +0100

    adding database

--- flask_app/config/mysqlconnection.py
+++ flask_app/config/mysqlconnection.py
@@ -8,7 +8,7 @@ class MySQLConnection:
         connection = pymysql.connect(
             host="localhost",
             user="root",
-            password="roo999t",
+            password="root",
             db=db,
             charset="utf8mb4",
             cursorclass=pymysql.cursors.DictCursor,
